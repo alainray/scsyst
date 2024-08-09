91f2387ab60d94c6b3c294c26acdeac122abb511
Alain <alainray@gmail.com>
2024-08-09 11:53:31 -0400

    Fixes

--- run_expt.py
+++ run_expt.py
@@ -1,8 +1,8 @@
 from args import args
 from train import run_experiment,save_features_best_model
-from utils import log_results, generate_random_seeds
+from utils import log_results, generate_random_seeds, save_model
 import time
-
+import argparse
 
 def run(args):
     seeds = generate_random_seeds(args.n_seeds)
@@ -17,7 +17,7 @@ def run(args):
         print(args)
         seed_start_time  = time.time()
         args.seed = seed
-        best_model, metrics, feats = run_experiment(args)
+        best_model, best_epoch, metrics, feats = run_experiment(args)
         # save metrics        
         for split in args.dataset_parameters.splits:
             log_results(args, metrics[split], split)
@@ -27,6 +27,8 @@ def run(args):
         seed_end_time  = time.time()
         # Calculate the elapsed time
         seed_elapsed_time = seed_end_time - seed_start_time
+        # save best model
+        save_model(args, best_model, best_epoch)
         print(f"[{i+1}] Elapsed time for seed {seed}: {seed_elapsed_time:.1f} seconds")
     end_time = time.time()
     elapsed_time = end_time - start_time
@@ -34,4 +36,23 @@ def run(args):
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process some arguments.")
+
+    # Add an argument
+    parser.add_argument(
+        "--n_filters", 
+        type=int, 
+        help="Number of filters in conv layer", default=256
+    )
+    parser.add_argument(
+        "--hidden_dim", 
+        type=int, 
+        help="Number of dimensions of representation", default=100
+    )   
+
+
+    # Parse the arguments
+    input_args = parser.parse_args()
+    args.filters = input_args.n_filters
+    args.hidden_dim = input_args.hidden_dim
     run(args)
